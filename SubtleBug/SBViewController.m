f22e1828b87d18b8036879f886bf83ca9ab3fe17
Kenneth Laskoski <kennethlaskoski@gmail.com>
2013-09-09 19:47:26 -0300

    SubtleBug 1.0

--- SubtleBug/SBViewController.m
+++ SubtleBug/SBViewController.m
@@ -8,7 +8,32 @@
 
 #import "SBViewController.h"
 
+const int a = 1024;
+const int s = a*a;
+
+int l(int i, int j)
+{
+    return i*a + j;
+}
+
+int m(int l)
+{
+    return l/a;
+}
+
+int n(int l)
+{
+    return l%a;
+}
+
 @interface SBViewController ()
+{
+    int data[s];
+}
+
+@property (weak, nonatomic) IBOutlet UITextField *first;
+@property (weak, nonatomic) IBOutlet UITextField *second;
+@property (weak, nonatomic) IBOutlet UILabel *result;
 
 @end
 
@@ -17,13 +42,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    for (int i = 0; i < a; ++i)
+        for (int j = 0; j < a; ++j)
+        {
+            int k = l(i,j);
+            data[k] = -k;
+        }
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)run:(id)sender
+{
+    int i = [self.first.text intValue];
+    int j = [self.second.text intValue];
+    if ((i < 0 || i >= a) || (j < 0 || j >= a))
+        NSLog(@"undefined behavior...");
+    int k = l(i,j);
+    int local[s];
+    local[k] = -data[k];
+    self.result.text = [NSString stringWithFormat:@"%d %d %d %d %d", m(k), n(k), k, data[k], local[k]];
 }
 
 @end
